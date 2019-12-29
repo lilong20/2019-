5ac059ce82f59be6c060350a1e7920518d2fd5f3
caidashuai <3295577683@qq.com>
2019-12-29 15:13:58 +0800

    登陆接口

--- final/src/main/java/edu/swjtuhc/demo/FinalApplication.java
+++ final/src/main/java/edu/swjtuhc/demo/FinalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
-
+ 
 public class FinalApplication {
 
 	public static void main(String[] args) {
